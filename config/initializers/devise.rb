e63c04cea3ad9e8dee36ae0cbad399a71f5ac993
Esteban Oliveros <esteban.jof@gmail.com>
2020-05-16 15:33:45 -0300

    respond with json on authentication failure

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = []
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -299,5 +299,9 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+  end  
+
+  config.warden do |manager|
+    manager.failure_app = ApiFailureApp
   end  
 end
